1ee7874cc2c1300a2706d9e853f571ea3607dac2
eskokado <eskokado@gmail.com>
2023-02-10 07:55:09 -0300

    test: linear search gem

---
+++ spec/algorithm/linear_search_spec.rb
@@ -0,0 +1,30 @@
+require 'rspec'
+require_relative './../../lib/algorithm/linear_search'
+RSpec.describe MyStudyGemPlan::LinearSearch do
+  value = 20
+  array = [10, 50, 30, 70, 80, 60, 20, 90, 40]
+  linearSearch = MyStudyGemPlan::LinearSearch.new
+  it "ensure search element #{value} is found" do
+    result = linearSearch.search_array(array, value)
+    expect(result).to eq("Target element is found")
+  end
+
+  value_invalid = 75
+  it "Ensure Search element(#{value_invalid}) result not found" do
+    result = linearSearch.search_array(array, value_invalid)
+    expect(result).to eq("Element is not found")
+  end
+
+  value_valid = 110
+  array = [10, 20, 80, 30, 60, 50, 110, 100, 130, 170]
+  it "ensure found #{value_valid} in array result index and element" do
+    result = linearSearch.search_array_index(array, value_valid)
+    expect(result).to eq("Element 110 is present at index 6")
+  end
+
+  value_invalid = 175
+  it "ensure search by element #{value_invalid} is not present in array" do
+    result = linearSearch.search_array_index(array, value_invalid)
+    expect(result).to eq("Element 175 is not present in array")
+  end
+end
--- spec/my_study_gem_plan_spec.rb
+++ spec/my_study_gem_plan_spec.rb
@@ -2,8 +2,4 @@ RSpec.describe MyStudyGemPlan do
   it "has a version number" do
     expect(MyStudyGemPlan::VERSION).not_to be nil
   end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
 end
