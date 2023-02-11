99b8ce30c4fcb18a2a3465bc54935b6a6e654040
eskokado <eskokado@gmail.com>
2023-02-11 05:39:38 -0300

    refactor: fix error

--- Gemfile
+++ Gemfile
@@ -4,5 +4,4 @@ source "https://rubygems.org"
 gemspec
 gem 'rspec', '~> 3.0'
 gem "rake", "~> 12.0"
-gem "rspec", "~> 3.0"
 
--- lib/my_study_gem_plan.rb
+++ lib/my_study_gem_plan.rb
@@ -1,7 +1,59 @@
 require "my_study_gem_plan/version"
-require_relative '../lib/algorithm/linear_search'
+# require_relative '../lib/algorithm/search.rb_copy'
+# require_relative '../lib/algorithm/search.rb_'
 module MyStudyGemPlan
-  class Error < StandardError; end
-  # Your code goes here...
-  class MyLinearSearch < LinearSearch; end
+  # class Error < StandardError; end
+  # # Your code goes here...
+  # class MyLinearSearch < LinearSearch.rbC; end
+  #
+  # class MyBinarySearch < BinarySearch.; end
+  module Algorithm
+    class LinearSearch
+      def search_array(arr, value_search)
+        message = "Element is not found"
+        for value in arr do
+          if value == value_search
+            message = "Target element is found"
+            break
+          end
+        end
+        return message
+      end
+
+      def search_array_index(arr, value_search)
+        message = "Element #{value_search} is not present in array"
+        arr.each_with_index do |value, index|
+          if value == value_search
+            message = "Element #{value_search} is present at index #{index}"
+            break
+          end
+        end
+        return message
+      end
+    end
+
+    class LinearSearch
+      def search_array(arr, value_search)
+        message = "Element is not found"
+        for value in arr do
+          if value == value_search
+            message = "Target element is found"
+            break
+          end
+        end
+        return message
+      end
+
+      def search_array_index(arr, value_search)
+        message = "Element #{value_search} is not present in array"
+        arr.each_with_index do |value, index|
+          if value == value_search
+            message = "Element #{value_search} is present at index #{index}"
+            break
+          end
+        end
+        return message
+      end
+    end
+  end
 end
